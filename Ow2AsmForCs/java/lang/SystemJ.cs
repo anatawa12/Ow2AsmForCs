393ad377b0102f6bb14b16a2232f8b8fc602baeb
anatawa12 <anatawa12@icloud.com>
2020-02-10 15:43:11 +0900

    impl arraycopy

--- Ow2AsmForCs/java/lang/SystemJ.cs
+++ Ow2AsmForCs/java/lang/SystemJ.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.CompilerServices;
 using org.objectweb.asm;
 
@@ -7,7 +8,29 @@ namespace java.lang
     {
         public static void arraycopy<T>(T[] src, int srcPos, T[] dst, int dstPos, int length)
         {
-            
+            if (src == null) 
+                throw new ArgumentNullException(nameof(src));
+            if (dst == null) 
+                throw new ArgumentNullException(nameof(src));
+            if (srcPos < 0) 
+                throw new IndexOutOfRangeException($"{nameof(srcPos)} < 0");
+            if (dstPos < 0) 
+                throw new IndexOutOfRangeException($"{nameof(dstPos)} < 0");
+            if (src.Length < srcPos+length) 
+                throw new IndexOutOfRangeException($"{nameof(src)}.{src.Length} < {nameof(srcPos)}+{nameof(length)}");
+            if (dst.Length < dstPos+length)
+                throw new IndexOutOfRangeException($"{nameof(dst)}.{dst.Length} < {nameof(dstPos)}+{nameof(length)}");
+
+            if (!ReferenceEquals(dst, src) || srcPos < dstPos)
+            {
+                for (int i = 0; i < length; i++)
+                    dst[srcPos + i] = src[dstPos + i];
+            }
+            else
+            {
+                for (int i = length - 1; 0 <= i; i--)
+                    dst[srcPos + i] = src[dstPos + i];
+            }
         }
 
         public static int identityHashCode(object label)
