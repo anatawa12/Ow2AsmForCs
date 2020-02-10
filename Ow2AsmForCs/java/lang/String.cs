b2b62e64f529456cf3c91a0d37510dc8001bd975
anatawa12 <anatawa12@icloud.com>
2020-02-10 12:11:05 +0900

    add String.substring

--- Ow2AsmForCs/java/lang/String.cs
+++ Ow2AsmForCs/java/lang/String.cs
@@ -8,5 +8,7 @@ namespace java.lang
         public static char[] toCharArray(this string _string) => _string.ToCharArray();
         public static char charAt(this string _string, int index) => _string[index];
         public static int indexOf(this string _string, char search, int startIndex = 0) => _string.IndexOf(search);
+        public static string substring(this string _string, int begin) => _string.Substring(begin);
+        public static string substring(this string _string, int begin, int end) => _string.Substring(begin, begin + end);
     }
 }
